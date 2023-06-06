16f4c368728750f56ca787d2c9bfde04802afd58
ロペズ ギヨム <guillaumelopezgk@hotmail.com>
2023-06-06 18:00:21 +0900

    project setup and nav

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./styles.css"
+import Navigation from './component/navigation/Navigation';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navigation />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/navigation/Navigation.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import "./navigation.css"
+
+const navData = ["abilities", "character", "customization", "Inventory", "Quests"]
+
+const Navigation = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleItemClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const handlePreviousClick = () => {
+    setActiveIndex((prevIndex) => {
+      if (prevIndex === null || prevIndex === 0) {
+        return navData.length - 1;
+      } else {
+        return prevIndex - 1;
+      }
+    });
+  };
+
+  const handleNextClick = () => {
+    setActiveIndex((prevIndex) => {
+      if (prevIndex === null || prevIndex === navData.length - 1) {
+        return 0;
+      } else {
+        return prevIndex + 1;
+      }
+    });
+  };
+
+  return (
+    <nav>
+      <div className="nav-container">
+        <button onClick={handlePreviousClick}>L1</button>
+        <ul className="nav-list">
+          {navData.map((nav, index) => (
+            <li
+              key={index}
+              className={index === activeIndex ? "active" : ""}
+              onClick={() => handleItemClick(index)}
+            >
+              {nav}
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleNextClick}>R1</button>
+      </div>
+      <div className='name-container'>
+        <h1>Guillaume Lopez |  </h1>
+        <h2>F-E DEV</h2>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
